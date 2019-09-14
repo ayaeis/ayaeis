f94402296ee60332be4ca5fb9e150e9c6d6a55e7
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-09-14 17:40:38 +0200

    Update First.java

--- First/src/First.java
+++ First/src/First.java
@@ -4,10 +4,13 @@ public class First {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(system.in); 
-		System.out.printin("Vad heter du?");
+		
 		
 		string str = input.nextlne();
-		System.out.printin(str);
+		System.out.printin(vad heter du?);
+		
+	
+		
 		
 	
 		
