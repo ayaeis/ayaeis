f2771163199de5a841c1c674b30cfa469d0b0c24
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-09-23 14:01:43 +0200

    Update Läxav38.java

--- First/Vecka 38/Läxav38.java
+++ First/Vecka 38/Läxav38.java
@@ -1,7 +1,37 @@
+import org.omg.CORBA.PRIVATE_MEMBER;
 
 public class Läxav38 {
-	public static void main(String[] args) {
-		
+	
+	Scanner input = new Scanner(System.in);
+	int highest = 0;
+	int lowest = 0;
+	double average = 0
+		   
+			System.out.print("Skriv in tio heltal: ");
+
+	for(int i = 0; i < 10; i++) {
+
+		int tempNumber = input.nextInt();
+
+		if(i == 0) {
+			highest = tempNumber;
+			lowest = tempNumber;
+		}
+
+		else if(tempNumber > highest) {
+			highest = tempNumber;
+		}
+
+		else if(tempNumber < lowest) {
+			lowest = tempNumber;
+		}
+
+		average = average + tempNumber;
+
+	}
+
+		   
+	
 	}
 
 }
