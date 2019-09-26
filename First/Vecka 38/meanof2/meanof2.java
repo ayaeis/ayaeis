91305665045a914be012086a9056cb63b0145724
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-09-26 10:38:21 +0200

    Create meanof2.java

---
+++ First/Vecka 38/meanof2/meanof2.java
@@ -0,0 +1,19 @@
+package meanof2;
+
+import java.util.Scanner;
+
+public class meanof2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int R1 = input.nextInt();
+		int S = input.nextInt();
+		
+		int R2;
+		
+		 R2 = 2*S - R1;
+		 System.out.println(R2);
+				
+	}
+	
+}
