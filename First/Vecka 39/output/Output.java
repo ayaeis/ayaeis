5f672b2e3342bb3144ad746c09c7fc315b083504
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-09-28 17:04:38 +0200

    Create Output.java

---
+++ First/Vecka 39/output/Output.java
@@ -0,0 +1,14 @@
+package output;
+
+import java.util.Scanner;
+
+public class Output {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt();
+		for ( int index = 1 ; index <= x; index++ ) {
+			System.out.println(index + "Abradacabra");
+		}
+	}
+
+}
