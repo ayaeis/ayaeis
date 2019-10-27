7a4945474f223028df589fce7b6cb5f980dd388f
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-10-27 15:58:05 +0100

    Update Hemlaxav43.java
    Metod 1, volym

--- First/AiehEissa/src/Hemlaxav43.java
+++ First/AiehEissa/src/Hemlaxav43.java
@@ -1,4 +1,14 @@
 
 public class Hemlaxav43 {
 
+	public static void main(String[] args) {
+
+		System.out.println(volume(2.16));
+		
+	}
+	public static double volume(double radius) {
+		double volume =  4/3 * radius * radius *radius * 3.14;
+		return volume;
+	}
+
 }
