5689ef7c9ea50b8f484a78db8c58504f9e14f7cb
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-11-18 11:00:03 +0100

    metod 1,2,3

--- First/src/Projekt.java
+++ First/src/Projekt.java
@@ -3,7 +3,25 @@ public class Projekt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(fahrenheitToCelsius(50));
+		System.out.println(kelvinToCelsius(0));
+		System.out.println(fluidPressure(FluidTable.WATER, 10));
+		
 
 	}
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		double celsius = (Farhenheit - 32)*5 / 9;
+		return celsius;
+	}
+	public static double kelvinToCelsius(double kelvin) {
+		double celsius = kelvin - 273.15; 
+		return celsius;
+	}
+	public static double fluidPressure(FluidTable fluid, double deep) {
+		double thePressure = 0;
+		thePressure = fluid.density*9.82*deep;
+		return thePressure;
+	}
+	
 
 }
