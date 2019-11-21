4229600735cab9946502e1483c8a025563738701
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-11-21 16:26:44 +0100

    Update Projekt.java
    Har lagt till javadoc.

--- First/src/Projekt.java
+++ First/src/Projekt.java
@@ -21,49 +21,115 @@ public class Projekt {
 		System.out.println(volumeToMass2(GasTable.AIR,1));
 		
 	}
+	/**
+	 * En metod som ändrar fahrenheit till celsius med en formel.
+	 * 
+	 * @param Fahrenheit värdet för fahrenehit.
+	 * @return Returnerar temperaturen i celsius.
+	 */
 	public static double fahrenheitToCelsius(double fahrenheit) {
-		double celsius = (Farhenheit - 32)*5 / 9;
+		double celsius = (fahrenheit - 32)*5 / 9;
 		return celsius;
 	}
+	/**
+	 * En metod som omvandlar kelvin till celsius med en formel.
+	 * 
+	 * @param Kelvin värdet för kelvin.
+	 * @return Returnerar temperaturen i celsius.
+	 */
 	public static double kelvinToCelsius(double kelvin) {
 		double celsius = kelvin - 273.15; 
 		return celsius;
 	}
+	/**
+	 * En metod som räknar ut vätsketrycket i en viss vätska under ett visst djup med en formel.
+	 * 
+	 * @param fluid Värdet för densiteten av en vätska 
+	 * @param deep värdet för djupet. Just nu är det 10 meter under vattnet.
+	 * @return
+	 */
 	public static double fluidPressure(FluidTable fluid, double deep) {
 		double thePressure = 0;
 		thePressure = fluid.density*9.82*deep;
 		return thePressure;
 	}
+	/**
+	 * En metod som räknar ut vätsketrycket i vatten vid ett viss djup, samma formel som förra metod.
+	 * 
+	 * @param deep Djupet man mäter trycket i.
+	 * @return returnerar vätsketycket.
+	 */
 	public static double pressureUnderWater(double deep) {
 		double underwaterPressure = 0;
 	    underwaterPressure = H20.density*9.82*deep;
 	    return underwaterPressure;
 	}
+	/**
+	 * Denna metod räknar ut kinetiska energin med hjälp av en formel.
+	 * 
+	 * @param mass Massan på ett objekt.
+	 * @param velocity Hastigheten på ett objekt.
+	 * @return Returnerar den kinetiska energin. 
+	 */
 	public static double kineticEnergy(double mass, double velocity) {
 		double kineticEnergy = 0;
 		kineticEnergy = (mass*velocity*velocity)/ 2;
 		return kineticEnergy;
 	}
+	/**
+	 * Denna metod räknar ut den potentiella energin på ett objekt. 
+	 * 
+	 * @param mass Massan på objektet.
+	 * @param height Höjden som objektet befinner sig på i förhållande till nollpunkten.
+	 * @return Returnerar den potentionella energin objektet har. 
+	 */
 	public static double potentialEnergy(double mass, double height) {
 		double potentialEnergy = 0;
 		potentialEnergy = mass*G*height;
 		return potentialEnergy;
 	}
+	/**
+	 * Metoden räknar ut den maximala hastigheten som ett objekt kommer att ha när den släpps från en viss höjd.
+	 * 
+	 * @param height Höjden objektet släpps ifrån.
+	 * @return Returnerar hastigheten.
+	 */
 	public static double fallSpeed(double height) {
 		double fallSpeed = 0;
 		fallSpeed = Math.sqrt(2*G*height);
 		return fallSpeed;
 	}
+	/**
+	 * En metod som räknar ut skillnaden mellan två värden.
+	 * 
+	 * @param first Det första vädret.
+	 * @param last Andra värdet.
+	 * @return Returnerar skillnaden mellan värdena.
+	 */
 	public static double delta(double first, double last) {
 		double delta = 0;
 		delta = last - first;
 		return delta;
 	}
+	/**
+	 * En metod som omvandlar en vätskas volym till vad dens massa.
+	 * 
+	 * @param fluid Vätskan.
+	 * @param volume Volymen.
+	 * @return Retunerar västkans massa.
+	 */
 	public static double volumeToMass(FluidTable fluid, double volume) {
 		double fluidMass = 0;
 		fluidMass = volume*fluid.density;
 		return fluidMass;
 	}
+	/**
+	 * En metod som räknar ut vad en gas volym väger i massa. 
+	 * 
+	 * @param gas Gasen.
+	 * @param volume Gasens volym.
+	 * @return Returnerar gasens massa. 
+	 */
 	public static double volumeToMass2(GasTable gas, double volume) {
 		double gasMass = 0;
 		gasMass = volume*gas.density;
