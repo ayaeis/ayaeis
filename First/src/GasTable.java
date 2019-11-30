62128129dfb3216376c32c8c81c2fad9913b7bbd
ayaeis <54796973+ayaeis@users.noreply.github.com>
2019-11-30 16:40:02 +0100

    har skrivit 3 nya ämne

--- First/src/GasTable.java
+++ First/src/GasTable.java
@@ -3,7 +3,9 @@ public enum GasTable{
 
 	AIR(1.29, 1.01), 
 	O2(1.43, 0.92), 
-	OXYGEN(1.43, 0.92)
+	OXYGEN(1.43, 0.92),
+	He(0.178, 5.2), //Helium
+    C3H8(2.02,1.53) //Propan
 	;
 
 	double density;
