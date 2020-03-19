eb070be04d18936f432ef08cac4b509c9fa1a6bd
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-03-19 16:29:54 +0100

    jag la till variablerna som jag kommer att använda under hela klassen, jag skrev metoder för både den svåra spelet och medium spelet, samt ltie javadoc. Jag skrev också en metod för alla tre svårighetsgrader sitället för att skriva samma sak flera gånger

--- First/src/gissaTalet.java
+++ First/src/gissaTalet.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 public class gissaTalet {
 
 	private static Scanner input;
-	public static void main(String[] args) {
-		input = new Scanner(System.in);
+	public static Int answer1; // för att se till om man vill fortsätta med spelet.
+	public static Int theLevel; // väljer nivån man vill ha.
+	public static Int minimumNumber; // minsta talet inom intervallet.
+	public static Int maximumNumber; // största talet inom intervallet.
+	public static Int howManyGuesses; // antalet gissningar spelaren vill ha.
+	public static Int theAnswer; // talet som slumpas fram och är det rätta svaret.
+	public static Int theGuessedNumber; // spelarens gissning.
+	public static Int guessesLeft; // hur många gissningar är kvar.
 
+
+	
+	public static void main(String[] args) {
+		guessGame();
 	}
 	/**
 	 * This method creates the answer by generating a random number.
@@ -19,6 +29,9 @@ public class gissaTalet {
 		return theAnswer.nextInt(maximumNumber - minimumNumber + 1) + minmumNumber; 
 		// (max - min + 1) + min för att vi skall kunna få 
 	}
+	/**
+	 * This method contains the coding needed to launch the game
+	 */
 	public static void guessGame() {
 		do {
 			while(run){
@@ -34,7 +47,7 @@ public class gissaTalet {
 				"1. Easy" + "2. Medium" + "3. Hard");
 					int theLevel = input.nextInt();
 					switch (theLevel) {
-					case 1: 
+					case 1:
 						easyMode();
 						break;
 						
@@ -52,17 +65,74 @@ public class gissaTalet {
 		}
 		
 	}
+	/**
+	 * This method contains the easy level of the game
+	 */
 	public static void easyMode() {
 		System.out.flush();
 		System.out.println("Write the minimum number of the interval");
 		int minimumNumber = input.nextInt();
 		System.out.println("Choose the maximum number of the interval");
 		int maximumNumer = input.nextInt();
-		System.out.println("You choose an interval between" + minimumNumber + "-" maximumNumber + "and you have unlimited guesses!");
+		System.out.println("You choose an interval between" + minimumNumber + "-" maximumNumber + "and you have 1000 guesses!");
 		System.out.println("Goodluck!");
-		
+		int howManyGuesses = 1000;
+		theWayTheGameWorks();
 		
 	}
-	
-
+	/**
+	 * This method contains the medium level of the game
+	 */
+	public static void mediumMode() {
+		System.out.flush();
+		System.out.println("Please choose the minmum number of the interval");
+		int minimumNumber = input.nextInt();
+		System.out.println("Please choose the maximum number of the interval");
+		int maximumNumber = input.nextInt();
+		System.out.println("Please choose how many guesses you want to have");
+		int howManyGuesses = input.nextInt();
+		System.out.println("Your number is between" + minimumNumber + "-" + maximumNumber + "and you have" + howManyGuesses + "guesses!");
+		
+		theWayTheGameWorks();
+	}
+	/**
+	 * This method contains the hard level of the game
+	 */
+	public static void hardMode() {
+		System.out.flush();
+		int maximumNumber = 1000;
+		int minimumNumber = 400;
+		int howManyGuesses = 5;
+		System.out.println("Choose a number larger than" + maximumNumber + "and you have" + howManyGuesses + "guesses");
+		theWayTheGameWorks();
+	}
+	public static void theWayTheGameWorks() {
+		System.out.println("Write your first guess!");
+		theAnswer = theRightAnswer(maximumNumber, minimumNumber);
+		
+		for (int 1 = 1; 1<= howManyGuesses; i--) {
+			int theGuessedNumber = input.nextInt();
+			howManyGuesses--;
+			
+			if (theGuessedNumber == theAnswer) {
+				System.out.println("You won!");
+				break;
+			}
+			else if (howManyGuesses != 0) {
+				if (theGuessedNumber > theAnswer) {
+					System.out.println("You guessed wrong! Try a lower number");
+					System.out.println("You have " + howManyGuesses " guesses left");
+					
+				}
+				else if (theGuessedNumber < theAnswer) {
+					System.out.println("You guessed wrong! Try a higher numebr");
+					System.out.println("You have" + howManyGuesses + "guesses left");
+				}
+			}
+			else {
+				System.out.println("You lost!");
+				break;
+			}
+		}
+	}
 }
