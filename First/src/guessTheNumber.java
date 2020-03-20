47bb9a89e149dc6fb28714f38e2129f6f312ae6e
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-03-20 19:48:38 +0100

    skrev koden för den medium svårighetsgraden

--- First/src/guessTheNumber.java
+++ First/src/guessTheNumber.java
@@ -51,7 +51,23 @@ public class guessTheNumber {
 		allModes();
 		
 	}
+	/**
+	 * This method contains the code for the easy level of this game.
+	 */
 	public static void MediumMode() {
+		System.out.flush();
+		System.out.println("You have chosen the medium level of the game!");
+		System.out.println("You get to choose the interval of the number and how many guesses you want!");
+		System.out.println();
+		System.out.println("Please write the minimum number of your desired interval");
+		minimumNumber = input.nextInt();
+		System.out.println("Now please write the maximum number of your desired interval");
+		maximumNumber = input.nextInt();
+		System.out.println("Now choose how many guesses you would like to have!");
+		howManyGuesses = input.nextInt();
+		System.out.println("Your interval is between" + minimumNumber + "and" + maximumNumber + "and you have" + howManyGuesses + "guesses!");
+		
+		allModes();
 		
 	}
 	public static void hardMode() {
