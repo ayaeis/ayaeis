3f67f6e24c532589630257b8f2c2ab8af027827a
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-03-20 18:08:55 +0100

    jag har skapat en scanner och skrivit de variablerna som jag ska kunna komma åt från alla metoder i klassen

--- First/src/guessTheNumber.java
+++ First/src/guessTheNumber.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 
 public class guessTheNumber {
+	static Scanner input = new Scanner(System.in);
+	
+	public static int answer1;
+	public static int theLevel; 
+	public static int minimumNumer; 
+	public static int maximumNumber;
+	public static int howManyGuesses;
+	public static int theAnswer;
+	public static int theGuessedNumber;
+	public static int guessesLeft;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
