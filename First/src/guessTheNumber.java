eefbabed8817b6dde9697fd611e9754374d46ab0
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-03-20 18:48:05 +0100

    förklarade i kommentarerna vad variablerna /koden är för.  skrev koden för den enkla svårighetsgraden + lade till en till metod som inte ingår i min planering men som jag kommer behöva

--- First/src/guessTheNumber.java
+++ First/src/guessTheNumber.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class guessTheNumber {
 	static Scanner input = new Scanner(System.in);
 	
-	public static int answer1;
-	public static int theLevel; 
-	public static int minimumNumber; 
-	public static int maximumNumber;
-	public static int howManyGuesses;
-	public static int theAnswer;
-	public static int theGuessedNumber;
-	public static int guessesLeft;
+	public static int answer1; //The answer to the question asking the player if they want to play or not
+	public static int theLevel; // The level the player chooses
+	public static int minimumNumber; // The minimum number in the chosen interval
+	public static int maximumNumber; // The maximum number in the chosen interval
+	public static int howManyGuesses; //How many guesses the player gets
+	public static int theAnswer; // The right answer which the player has to guess
+	public static int theGuessedNumber; // The number that the player guessed
+	public static int guessesLeft; // How many guesses left the player has after x guesses
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 	/**
@@ -28,18 +29,35 @@ public class guessTheNumber {
 		Random theAnswer = new Random();
 		
 		return theAnswer.nextInt(maximumNumber - minimumNumber + 1) + minimumNumber;
-		
+		// (max - min + 1) + min so that the generated answer would be >=maximumNumber
 	}
 	public static void beginGuessGame() {
 		
 	}
+	/**
+	 * This method contains the code for the easy level of this game.
+	 */
 	public static void easyMode() {
+		System.out.flush();
+		System.out.println("You have chosen the easy level of the game!");
+		System.out.println("You get to choose the interval of the number and you have 1000 guesses!");
+		System.out.println("Please write the minimum number of your desired interval");
+		minimumNumber = input.nextInt();
+		System.out.println("Now please write the maximum number of your desired interval");
+		maximumNumber = input.nextInt();
+		System.out.println("Your interval is between" + minimumNumber + "and" + maximumNumber +"and you have 1000 guesses!" );
+		howManyGuesses = 1000;
+		
+		allModes();
 		
 	}
 	public static void MediumMode() {
 		
 	}
 	public static void hardMode() {
+		
+	}
+	public static void allModes() {
 		
 	}
 
