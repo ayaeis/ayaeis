9d34f0213f1ee9c80d7a305ce43760b1c04fa01d
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-03-20 17:37:03 +0100

    jag har börjat med att skriva metoderna som jag, enligt min planering, kommer behöva

---
+++ First/src/guessTheNumber.java
@@ -0,0 +1,25 @@
+
+public class guessTheNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static int theRightAnswer() {
+		return 0;
+		
+	}
+	public static void beginGuessGame() {
+		
+	}
+	public static void easyMode() {
+		
+	}
+	public static void MediumMode() {
+		
+	}
+	public static void hardMode() {
+		
+	}
+
+}
