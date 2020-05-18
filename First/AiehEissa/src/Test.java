95e05b72d62dca6ae37be85d699ed16d81af449d
ayaeis <54796973+ayaeis@users.noreply.github.com>
2020-05-18 23:08:57 +0200

    x

---
+++ First/AiehEissa/src/Test.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		char newchar = input.next().charAt(0);
+		boolean isDigit = Character.isDigit(newchar);
+		if(isDigit) {
+			System.out.println(newchar);
+		}
+		else {
+			System.out.println("n");
+		}
+	}
+}
+
